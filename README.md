6537083858447a767483a2e2732b4d154ce5f749
pedro-nguinamau <nguinamaupedro51@gmail.com>
2024-12-21 02:00:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Server-Http-Typescript-Fastify
---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import fastify from "fastify";
+
+const server = fastify({ logger: true })
+
+server.get("/ping", async (request, reply) => {
+  return { message: "pong" };
+});
+
+
+
+server.listen({port: 8080}, (err, address) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      console.log(`Server listening on address ${address}`);
+});
+    
